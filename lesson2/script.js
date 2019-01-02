add2ad1d905b8743a7f4cc65168dbf00bf8e7095
Pavel <panda8808@mail.ru>
2019-01-02 12:06:06 +0300

    выполнено четвертое задание

--- lesson2/script.js
+++ lesson2/script.js
@@ -14,3 +14,25 @@ if (a >= 0 && b >= 0) {
 } else {
   document.write(`<p>Результат: ${a+b}</p>`);
 }
+
+//четвертое задание
+document.write("<h2>Четвертое задание</h2>");
+a = Math.round(Math.random()*15);
+switch (a) {
+  case 0: document.write("0 <br>");
+  case 1: document.write("1 <br>");
+  case 2: document.write("2 <br>");
+  case 3: document.write("3 <br>");
+  case 4: document.write("4 <br>");
+  case 5: document.write("5 <br>");
+  case 6: document.write("6 <br>");
+  case 7: document.write("7 <br>");
+  case 8: document.write("8 <br>");
+  case 9: document.write("9 <br>");
+  case 10: document.write("10 <br>");
+  case 11: document.write("11 <br>");
+  case 12: document.write("12 <br>");
+  case 13: document.write("13 <br>");
+  case 14: document.write("14 <br>");
+  case 15: document.write("15 <br>");
+}
