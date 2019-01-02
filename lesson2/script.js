07bf83f5e82b8b97b233f26c183ce87f353767a3
Pavel <panda8808@mail.ru>
2019-01-02 11:55:48 +0300

    выполнено третье задание

--- lesson2/script.js
+++ lesson2/script.js
@@ -3,3 +3,14 @@ var a = 2;
 var x = 1 + (a *= 2);
 document.write("<h2>Второе задание</h2>");
 document.write(`<p>Результат: ${x}</p>`);
+
+//третье задание
+var a = -1, b = -2;
+document.write("<h2>Третье задание</h2>");
+if (a >= 0 && b >= 0) {
+  document.write(`<p>Результат: ${a-b}</p>`);
+} else if (a < 0 && b < 0) {
+  document.write(`<p>Результат: ${a*b}</p>`);
+} else {
+  document.write(`<p>Результат: ${a+b}</p>`);
+}
