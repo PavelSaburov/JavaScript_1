0808ccb22b4b1e2ff488adcd1b00a25ad6c2e149
Pavel <panda8808@mail.ru>
2019-01-11 20:15:23 +0300

    описана функция проверки простое ли число. Добавлен вывод простых чисел на отрезке [0, 100]

--- lesson3/script.js
+++ lesson3/script.js
@@ -1,1 +1,18 @@
+//первое задание
+function isPrime (arg) {
+  if (arg < 2) {
+    return false;
+  }
+  for (let i = 2; i <= Math.round(Math.sqrt(arg)); i++) {
+    if (arg % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
 
+for (let i = 0; i <= 100; i++) {
+  if (isPrime(i)) {
+    console.log(i);
+  }
+}
