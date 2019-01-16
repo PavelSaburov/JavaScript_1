1402994fd2760126b9ec48d3b000b5f8b6542e79
Pavel <panda8808@mail.ru>
2019-01-16 16:10:43 +0300

    описан основной блок игры

--- lesson4/millioner/gameCode.js
+++ lesson4/millioner/gameCode.js
@@ -1,12 +1,21 @@
 var resp,
-  progress = [],
   money = 0,
   answer,
   questionCount = 0;
 alert('Добро подаловать в игру "Кто хочет стать миллионером"\nНажмите "ОК" как будете готовы');
 while (questionCount < questions.length) {
-  answer = getAnswer(questionCount);
-
+  do {
+    answer = getAnswer(questionCount);
+  } while (!isAnswer(answer));
+  if (answer === 5 || questionCount === questions.length-1) {
+    alert("Поздравляем. Вы выиграли: " + money);
+    break;
+  } else if (answer === questions[questionCount].trueAnsw) {
+    money += questions[questionCount].bonus;
+  } else {
+    alert("Увы. Вы проиграли");
+    break;
+  }
   questionCount++;
 }
 
@@ -16,7 +25,7 @@ function isAnswer(resp) {
         alert('Вы ввели недопустимый символ');
         return false;
     }
-    else if (resp < 1 || resp > 4) {
+    else if (resp < 1 || resp > 5) {
         alert('Ваше число выходит из допустимого диапозона');
         return false;
     }
@@ -27,6 +36,6 @@ function isAnswer(resp) {
 function getAnswer (numQuestion) {
   resp = +prompt("Внимание вопрос!\n" + questions[numQuestion].question + "Варианты ответов:\n"
          + questions[numQuestion].answ1 + questions[numQuestion].answ2 + questions[numQuestion].answ3
-         + questions[numQuestion].answ4 + "0: взять деньги");
+         + questions[numQuestion].answ4 + "5: взять деньги");
   return resp;
 }
