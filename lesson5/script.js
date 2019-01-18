d0405863e8f3b1ee5d94c9be2d2ce948255206c1
Pavel <panda8808@mail.ru>
2019-01-18 19:49:55 +0300

    описана функция создания таблицы

--- lesson5/script.js
+++ lesson5/script.js
@@ -1,3 +1,23 @@
-/**
- * Created by Pavel on 18.01.2019.
- */
+var rows = 8, cols = 8;
+
+function createTable() {
+
+  var table = document.createElement("table");
+  for (var i = 0; i <= rows+1; i++) {
+    var row = document.createElement("tr");
+    table.appendChild(row);
+    for (var j = 0; i <= cols+1; j++) {
+      var col = document.createElement("td");
+      col.content = "1";
+      if (j % 2 === 0) {
+        col.classList.add("black");
+      } else {
+        col.classList.add("white");
+      }
+      row.appendChild(col);
+    }
+  }
+  return table;
+}
+
+document.body.appendChild(createTable());
