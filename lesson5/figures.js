287effe954596ce6e8aa91f4c9f1a09cd159976d
Pavel <panda8808@mail.ru>
2019-01-20 19:36:34 +0300

    заменен вид фигурок

--- lesson5/figures.js
+++ lesson5/figures.js
@@ -19,38 +19,39 @@ function Figure (name, classColor, avatar) {
 
 }
 
-gameField[1][1] = new Figure("л", "blackFigure", "&#9820;");
-gameField[1][2] = new Figure("к", "blackFigure", "&#9822;");
-gameField[1][3] = new Figure("с", "blackFigure", "&#9821;");
-gameField[1][4] = new Figure("К", "blackFigure", "&#9818;");
-gameField[1][5] = new Figure("ф", "blackFigure", "&#9819;");
-gameField[1][6] = new Figure("с", "blackFigure", "&#9821;");
-gameField[1][7] = new Figure("к", "blackFigure", "&#9822;");
-gameField[1][8] = new Figure("л", "blackFigure", "&#9820;");
+gameField[1][1] = new Figure("л", "blackFigure", "♜");
+gameField[1][2] = new Figure("к", "blackFigure", "♞");
+gameField[1][3] = new Figure("с", "blackFigure", "♝");
+gameField[1][4] = new Figure("К", "blackFigure", "♚");
+gameField[1][5] = new Figure("ф", "blackFigure", "♛");
+gameField[1][6] = new Figure("с", "blackFigure", "♝");
+gameField[1][7] = new Figure("к", "blackFigure", "♞");
+gameField[1][8] = new Figure("л", "blackFigure", "♜");
 
-gameField[2][1] = new Figure("п", "blackFigure", "&#9823;");
-gameField[2][2] = new Figure("п", "blackFigure", "&#9823;");
-gameField[2][3] = new Figure("п", "blackFigure", "&#9823;");
-gameField[2][4] = new Figure("п", "blackFigure", "&#9823;");
-gameField[2][5] = new Figure("п", "blackFigure", "&#9823;");
-gameField[2][6] = new Figure("п", "blackFigure", "&#9823;");
-gameField[2][7] = new Figure("п", "blackFigure", "&#9823;");
-gameField[2][8] = new Figure("п", "blackFigure", "&#9823;");
+gameField[2][1] = new Figure("п", "blackFigure", "♟");
+gameField[2][2] = new Figure("п", "blackFigure", "♟");
+gameField[2][3] = new Figure("п", "blackFigure", "♟");
+gameField[2][4] = new Figure("п", "blackFigure", "♟");
+gameField[2][5] = new Figure("п", "blackFigure", "♟");
+gameField[2][6] = new Figure("п", "blackFigure", "♟");
+gameField[2][7] = new Figure("п", "blackFigure", "♟");
+gameField[2][8] = new Figure("п", "blackFigure", "♟");
 
-gameField[7][1] = new Figure("п", "whiteFigure", "&#9823;");
-gameField[7][2] = new Figure("п", "whiteFigure", "&#9823;");
-gameField[7][3] = new Figure("п", "whiteFigure", "&#9823;");
-gameField[7][4] = new Figure("п", "whiteFigure", "&#9823;");
-gameField[7][5] = new Figure("п", "whiteFigure", "&#9823;");
-gameField[7][6] = new Figure("п", "whiteFigure", "&#9823;");
-gameField[7][7] = new Figure("п", "whiteFigure", "&#9823;");
-gameField[7][8] = new Figure("п", "whiteFigure", "&#9823;");
+gameField[7][1] = new Figure("п", "whiteFigure", "♟");
+gameField[7][2] = new Figure("п", "whiteFigure", "♟");
+gameField[7][3] = new Figure("п", "whiteFigure", "♟");
+gameField[7][4] = new Figure("п", "whiteFigure", "♟");
+gameField[7][5] = new Figure("п", "whiteFigure", "♟");
+gameField[7][6] = new Figure("п", "whiteFigure", "♟");
+gameField[7][7] = new Figure("п", "whiteFigure", "♟");
+gameField[7][8] = new Figure("п", "whiteFigure", "♟");
 
-gameField[8][1] = new Figure("л", "whiteFigure", "&#9820;");
-gameField[8][2] = new Figure("к", "whiteFigure", "&#9822;");
-gameField[8][3] = new Figure("с", "whiteFigure", "&#9821;");
-gameField[8][4] = new Figure("К", "whiteFigure", "&#9818;");
-gameField[8][5] = new Figure("ф", "whiteFigure", "&#9819;");
-gameField[8][6] = new Figure("с", "whiteFigure", "&#9821;");
-gameField[8][7] = new Figure("к", "whiteFigure", "&#9822;");
-gameField[8][8] = new Figure("л", "whiteFigure", "&#9820;");
+gameField[8][1] = new Figure("л", "whiteFigure", "♜");
+gameField[8][2] = new Figure("к", "whiteFigure", "♞");
+gameField[8][3] = new Figure("с", "whiteFigure", "♝");
+gameField[8][4] = new Figure("К", "whiteFigure", "♚");
+gameField[8][5] = new Figure("ф", "whiteFigure", "♛");
+gameField[8][6] = new Figure("с", "whiteFigure", "♝");
+gameField[8][7] = new Figure("к", "whiteFigure", "♞");
+gameField[8][8] = new Figure("л", "whiteFigure", "♜");
+
