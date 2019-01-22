8b71d7c5c03f28d80eff49020129aab04ed3793e
Pavel <panda8808@mail.ru>
2019-01-22 20:04:10 +0300

    функция создания эскиза опробована в тестовом режиме. Исправлены ошибки кода и стилей

--- lesson6/gallery/script.js
+++ lesson6/gallery/script.js
@@ -1,12 +1,13 @@
 var countPictures = 4,
-    miniaturePath = "img/small",
-    picturePath = "img/big",
-    contMiniature = ".container";
+    miniaturePath = "img/small/",
+    picturePath = "img/big/",
+    contMiniature = ".miniature";
 function createMiniature (name) {
   var miniature = document.createElement("div");
   miniature.id = name;
   miniature.classList.add("miniature__item");
-  miniature.style.backgroundColor.uri = miniaturePath + name + ".jpg";
+  miniature.style.backgroundImage = "url(" + miniaturePath + name + ".jpg)";
+  console.log(miniature.style);
   return miniature;
 }
-
+document.querySelector(contMiniature).appendChild(createMiniature(0));
