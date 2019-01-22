fe4b14ad7ed22697bf6ee94f73d44f18b10103db
Pavel <panda8808@mail.ru>
2019-01-22 20:25:35 +0300

    создана отдельная функция добавления эскиза. Добавлены стили для большой картники

--- lesson6/gallery/script.js
+++ lesson6/gallery/script.js
@@ -1,17 +1,25 @@
 var countPictures = 4,
     miniaturePath = "img/small/",
     picturePath = "img/big/",
-    contMiniatureClass = ".miniature",
-    contMiniature = document.querySelector(contMiniatureClass);
+    miniatureClass = ".miniature",
+    contMiniature = document.querySelector(miniatureClass);
+    pictureClass = ".picture";
+    contPicture = document.querySelector(pictureClass);
+
 function createMiniature (name) {
   var miniature = document.createElement("div");
   miniature.id = name;
   miniature.classList.add("miniature__item");
-  miniature.style.backgroundImage = "url(" + miniaturePath + name + ".jpg)";
+  miniature.style.backgroundImage = setBackgroundImage(miniaturePath, name);
   return miniature;
 }
 
-
+function setBackgroundImage (path, namePic) {
+  return "url(" + path + namePic + ".jpg)"
+}
 for (var i = 0; i <= countPictures; i++) {
   contMiniature.appendChild(createMiniature(i));
+  if (i === 0) {
+    contPicture.style.backgroundImage = setBackgroundImage(picturePath, 0);
+  }
 }
