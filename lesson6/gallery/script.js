4ce833c3225d35d7c1e92e9228a7c14ee297632c
Pavel <panda8808@mail.ru>
2019-01-22 20:10:13 +0300

    описан цикл добавления эскизов на страницу

--- lesson6/gallery/script.js
+++ lesson6/gallery/script.js
@@ -1,13 +1,17 @@
 var countPictures = 4,
     miniaturePath = "img/small/",
     picturePath = "img/big/",
-    contMiniature = ".miniature";
+    contMiniatureClass = ".miniature",
+    contMiniature = document.querySelector(contMiniatureClass);
 function createMiniature (name) {
   var miniature = document.createElement("div");
   miniature.id = name;
   miniature.classList.add("miniature__item");
   miniature.style.backgroundImage = "url(" + miniaturePath + name + ".jpg)";
-  console.log(miniature.style);
   return miniature;
 }
-document.querySelector(contMiniature).appendChild(createMiniature(0));
+
+
+for (var i = 0; i <= countPictures; i++) {
+  contMiniature.appendChild(createMiniature(i));
+}
