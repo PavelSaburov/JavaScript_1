e07d96bea38adbcd3361f3dce193bf39ed7c3b7e
Pavel <panda8808@mail.ru>
2019-01-22 19:46:23 +0300

    описана функция создания эскизов

--- lesson6/gallery/script.js
+++ lesson6/gallery/script.js
@@ -1,1 +1,12 @@
+var countPictures = 4,
+    miniaturePath = "img/small",
+    picturePath = "img/big",
+    contMiniature = ".container";
+function createMiniature (name) {
+  var miniature = document.createElement("div");
+  miniature.id = name;
+  miniature.classList.add("miniature__item");
+  miniature.style.backgroundColor.uri = miniaturePath + name + ".jpg";
+  return miniature;
+}
 
